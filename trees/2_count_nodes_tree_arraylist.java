c9c8637a56a9d3330ce80d2899275f2905e81462
AdiTya-Dev31 <52311859+AdiTya-Dev31@users.noreply.github.com>
2019-12-13 21:48:40 +0530

    Create 2_count_nodes_tree_arraylist.java

---
+++ trees/2_count_nodes_tree_arraylist.java
@@ -0,0 +1,70 @@
+import java.util.*;
+
+class Treenode<T>{
+    T data;
+    ArrayList<Treenode<T>> children;
+    Treenode(T data){
+        this.data = data;
+        children = new ArrayList<>();
+    }
+}
+
+
+
+
+public class Main
+{
+    
+    ///  C O U N T    N O D E  ///
+    
+    public static int countNodes(Treenode<Integer> root){
+        int count = 1;
+        for(int i=0; i<root.children.size(); i++){
+            count += countNodes(root.children.get(i));
+        }
+        return count;
+    }
+    
+    
+    ///  I N P U T  ///
+    
+    public static Treenode<Integer> takeInput(Scanner s){
+        System.out.print("Enter data : ");
+        int x = s.nextInt();                        // getting data value
+        System.out.print("Enter children : ");
+        int y = s.nextInt();                        // getting no. of children 
+        Treenode<Integer> root = new Treenode<>(x); // taking root data
+        for(int i=0; i<y; i++){                     // taking root children
+            Treenode<Integer> child = takeInput(s); // making child
+            root.children.add(child);               // adding child
+        }
+        return root;
+    }
+    
+    
+    ///  P R I N T  ///
+    
+    public static void print(Treenode<Integer> root){
+        System.out.print(root.data + " : ");
+        int y = root.children.size();
+        String st = "";
+        for(int i=0; i<y; i++){
+            st = st + " " + root.children.get(i).data;
+        }
+        System.out.print(st);
+        System.out.println();
+        for(int i=0; i<y; i++){
+            print(root.children.get(i));
+        }
+    }
+    
+    
+    /// M A I N ///
+    
+	public static void main(String[] args) {
+	    Scanner s = new Scanner(System.in);
+	    Treenode<Integer> root = takeInput(s);
+		print(root);
+		System.out.println("No. of nodes : " + countNodes(root));//count number of nodes
+	}
+}
