0bad36b2cb70f033a97d3223c819b4bf1b440898
AdiTya-Dev31 <52311859+AdiTya-Dev31@users.noreply.github.com>
2019-12-21 15:23:35 +0530

    Update 1_Fibonacci_1.java

--- Dynamic_Prog_(DP)/1_Fibonacci_1.java
+++ Dynamic_Prog_(DP)/1_Fibonacci_1.java
@@ -14,27 +14,27 @@ public class Main
     
     // D P //
     
-    public static int fibo(int n){
-        int storage[] = new int[n+1];
+    public static int fibo(int n){        // say for fibo(4) we need fibo(4), fibo(3), 2, 1, 0
+        int storage[] = new int[n+1];     // hence make an empty array of size = n+1 to store values
         for(int i=0; i<n+1; i++){
-            storage[i] = -1;
+            storage[i] = -1;              // set each value = -1
         }
-        return fibo(n, storage);
+        return fibo(n, storage);          // send n and storage array to another fibo(int , int[])
     }
     
     
-    public static int fibo(int n, int storage[]){
-        if(n==0 || n==1){
-            if(storage[n]==-1)
-                storage[n] = n;
-            return storage[n];
+    public static int fibo(int n, int storage[]){ // strategy -> if values are already present in storage then no need to call the function
+        if(n==0 || n==1){                         // B A S E  C A S E 
+            if(storage[n]==-1)                    // check if present in storage or not ... if not present then put the value in storage
+                storage[n] = n;                   // put value in storage
+            return storage[n];                    // return the stored value
         }
-        if(storage[n]==-1)
-            storage[n] = fibo(n-1, storage)+fibo(n-2, storage);
-        return storage[n];
+        if(storage[n]==-1)                    		          // check if present in array or not
+            storage[n] = fibo(n-1, storage)+fibo(n-2, storage);   // if not then store
+        return storage[n];                                        // return the stored value
     }
     
 	public static void main(String[] args) {
-		System.out.println(fibo(55));
+		System.out.println(fibo(55));         // O(n)
 	}
 }
