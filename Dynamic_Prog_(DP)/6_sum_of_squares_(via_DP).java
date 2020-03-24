ab8aa1b24f190f259ba1c8131aa159c67b6a22f6
AdiTya-Dev31 <52311859+AdiTya-Dev31@users.noreply.github.com>
2020-03-24 18:26:25 +0530

    This Dynamic program would give sum of minimum no. of squares

--- Dynamic_Prog_(DP)/6_sum_of_squares_(via_DP).java
+++ Dynamic_Prog_(DP)/6_sum_of_squares_(via_DP).java
@@ -26,39 +26,28 @@
 
 
 
-import java.lang.Math;
-
-public class Main
-{
-    public static int sumOfSquares(int n){
-        int opts = (int)Math.sqrt(n);
-        int storage[] = new int[opts+1];
-        for(int i=0; i<=opts; i++){
-            storage[i] = (i*i);
-        }
-        return sumOfSquares(n, storage, opts);
+public class Main{
+    
+    public static int findMin(int a, int b){
+        int x= a<b?a:b;
+        return x;
     }
     
-    
-    public static int sumOfSquares(int n, int storage[], int opts){
-        int count = 0;
-        int i = opts;
-        int remaining = n;
-        while(remaining!=0){
-            for(i=i; i>=0; i--){
-                if(storage[i]<=remaining){
-                    count++;
-                    remaining = remaining - storage[i];   // if i = 5 here
-                    break;
-                }
-            }               
-                            // then i is also 5 here
-        }
-        return count;
+    public static int count(int n){
+        int store[] = new int[n+1];
+        store[1] = 1;
+        for(int i=2; i<=n; i++){
+            store[i] = i;           // using only 1
+            int min = store[i];
+            for(int j=1; j*j<=i; j++){ // ex - n = 30 then we need 25+5 = 5^2 + 2^2 + 1^2 = 3
+                store[i] = store[i-j*j]+1;
+                min = findMin(min, store[i]);
+                store[i] = min;
+            }
+        }return store[n];
     }
     
-    
-	public static void main(String[] args) {
-		System.out.println("min num are:\t" + sumOfSquares(30));
-	}
+    public static void main(String[] args){
+        System.out.println(count(14));          // 14=9+5== 3^2 + 2^2 + 1^2 == 1step + 2step
+    }
 }
