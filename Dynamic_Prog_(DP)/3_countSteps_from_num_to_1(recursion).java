685cdb5924de16e205118f42b9c2a5fe49510942
AdiTya-Dev31 <52311859+AdiTya-Dev31@users.noreply.github.com>
2020-03-24 19:13:14 +0530

    Update 3_countSteps_from_num_to_1(recursion).java

--- Dynamic_Prog_(DP)/3_countSteps_from_num_to_1(recursion).java
+++ Dynamic_Prog_(DP)/3_countSteps_from_num_to_1(recursion).java
@@ -5,29 +5,21 @@
 
 public class Main{
     
-    public static int count2one(int n){
-        if(n<=1)
-            return 0;
+    public static int findMin(int a, int b){ return a<b?a:b; }
+    
+    public static int count(int n){
+        int storage[] = new int[n+1];
+        for(int i=2; i<=n; i++){
             
-        int op1 = 1 + count2one(n-1);           // option 1
-        int minCounts = op1;
-        
-        if(n % 3 == 0){                         // option 2
-            int op2 = 1 + count2one(n/3);
-            if(op2<minCounts)
-                minCounts = op2;
-        }
-        
-        if(n % 2 == 0){                        // option 3
-            int op3 = 1 + count2one(n/2);
-            if(op3<minCounts)
-                minCounts = op3;
-        }
-        
-        return minCounts;
+            int min = storage[i-1];                           // option1
+            if(i%2==0){ min = findMin(min, storage[i/2]); }   // option2
+            if(i%3==0){ min = findMin(min, storage[i/3]); }   // option3
+            
+            storage[i] = min+1;    // setting storage[i]
+        } return storage[n];
     }
     
-    public static void main(String args[]){
-        System.out.println(count2one(10));   // solution   10 -> 9 -> 3 -> 1 (3 steps)
+    public static void main(String[] args){
+        System.out.println(count(11));
     }
 }
