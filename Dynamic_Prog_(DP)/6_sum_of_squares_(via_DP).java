62b2abe84fc9ae69e32eaeac46c61514e0b145d4
AdiTya-Dev31 <52311859+AdiTya-Dev31@users.noreply.github.com>
2020-03-24 19:27:35 +0530

    Update 6_sum_of_squares_(via_DP).java

--- Dynamic_Prog_(DP)/6_sum_of_squares_(via_DP).java
+++ Dynamic_Prog_(DP)/6_sum_of_squares_(via_DP).java
@@ -1,27 +1,24 @@
 
-
 public class Main{
     
-    public static int findMin(int a, int b){
-        int x= a<b?a:b;
-        return x;
-    }
+    public static int findMin(int a, int b){ return a<b?a:b; }
     
     public static int count(int n){
         int store[] = new int[n+1];
-        store[1] = 1;
+        store[1]=1;
         for(int i=2; i<=n; i++){
-            store[i] = i;           // using only 1
-            int min = store[i];
-            for(int j=1; j*j<=i; j++){ // ex - n = 30 then we need 25+5 = 5^2 + 2^2 + 1^2 = 3
-                store[i] = store[i-j*j]+1;
-                min = findMin(min, store[i]);
-                store[i] = min;
+            int min = i;   // using 1^2 only
+            for(int j=1; j*j<=i ; j++){     // perfect square + remaining
+                min = findMin(min, store[i-j*j]+1);  // renmaining ans = store[i-j*j]   and  perfectSq ans = 1
             }
-        }return store[n];
+            store[i] = min;     // min of both is set to store[i]
+        }
+        return store[n];
     }
     
     public static void main(String[] args){
-        System.out.println(count(14));          // 14=9+5== 3^2 + 2^2 + 1^2 == 1step + 2step
+        System.out.println(count(10));
     }
 }
+
+// ans = 3 (9+1 == 3^2 + 1^2 == perfectSq + remaining)
