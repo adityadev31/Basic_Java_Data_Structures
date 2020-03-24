e96e0c77040917f41243cb90c38ab83ac244c317
AdiTya-Dev31 <52311859+AdiTya-Dev31@users.noreply.github.com>
2020-03-24 18:26:58 +0530

    Update 6_sum_of_squares_(via_DP).java

--- Dynamic_Prog_(DP)/6_sum_of_squares_(via_DP).java
+++ Dynamic_Prog_(DP)/6_sum_of_squares_(via_DP).java
@@ -1,30 +1,4 @@
 
-/***
-
-        STRATEGY:-   if say num = 10  ->  1, 4, 9  can be used hence  number of opts are [sqrt(num)] here opt = 3
-        
-        step 1-   array storage[opts+1] = {0, 1, 4, 9}
-        
-        step 2-   int count, int remaining=n,  int i=opt {coz we will move from right to left of the array till remaining = 0}
-        
-        step 3-   while(remain != 0){
-            
-            step 4 -    we will check if storage[i] <= remaining
-            step 5 -    if(no then i--)
-            step 6 -    if(yes then) -->  count++;  remaining = remaining - (storage[i]);    break;
-                        
-                        i.e, remaining = 10 - 9   (for i=opt)
-                        i.e, count  =  1;
-                        i.e, break; (i will still remain = current i)
-            
-            step 7 -    next iteration (i = 3) and remaining = 1
-            step 8 -    now loop till storage[i] <= 1
-            step 9 -    count++  = 2;  break;
-        }
-        
-***/
-
-
 
 public class Main{
     
