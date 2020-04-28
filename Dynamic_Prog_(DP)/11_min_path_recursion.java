b68bd0e5574e2c39fbabbbaf0faf45a360c5767e
AdiTya-Dev31 <52311859+AdiTya-Dev31@users.noreply.github.com>
2020-04-28 10:04:35 +0530

    corrected

--- Dynamic_Prog_(DP)/11_min_path_recursion.java
+++ Dynamic_Prog_(DP)/11_min_path_recursion.java
@@ -3,14 +3,14 @@ import java.util.*;
 public class Main {
     
     
-    public static int findMinPath(int[][] maze, int i, int j, int sum, int rows, int cols){
-        if(i==rows-1 && j==cols-1){  return sum + maze[i][j]; } // base case
-        if(i>=rows || j>=cols){ return sum; }
-        int horiz = findMinPath(maze, i+1, j, sum, rows, cols);
-        int verti = findMinPath(maze, i, j+1, sum, rows, cols);
-        int diago = findMinPath(maze, i+1, j+1, sum, rows, cols);
+    public static int findMinPath(int[][] maze, int i, int j, int rows, int cols){
+        if(i==rows-1 && j==cols-1){  return maze[i][j]; }   // base case
+        if(i>=rows || j>=cols){ return Integer.MAX_VALUE; } // base case
+        int horiz = findMinPath(maze, i+1, j, rows, cols);
+        int verti = findMinPath(maze, i, j+1, rows, cols);
+        int diago = findMinPath(maze, i+1, j+1, rows, cols);
         int min = horiz;
-        min = horiz<verti ? horiz:verti;
+        min = min<verti ? min:verti;
         min = min<diago ? min:diago;
         return maze[i][j]+min;
     }
@@ -18,18 +18,17 @@ public class Main {
     public static int findMinPath(int[][] maze){
         int rows = maze.length;
         int cols = maze[0].length;
-        return findMinPath(maze, 0, 0, 0, rows, cols);
+        return findMinPath(maze, 0, 0, rows, cols);
     }
     
     public static void main(String[] args) throws Exception {
         int[][] maze = {
-            {1,2,4,8},
-            {9,5,1,6},
-            {3,2,4,5},
+            {1,1,1},
+            {4,5,2},
+            {7,8,9},
         };
         System.out.print("Minimum path = " + findMinPath(maze));
     }
 }
 
-
-// ans = 8  ==>  1->2->1->5
+// ans = 13 ==> 1->1->2->9
