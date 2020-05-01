7b26a92bc8460eaf8b70c5efdf93bf46f5398633
AdiTya-Dev31 <52311859+AdiTya-Dev31@users.noreply.github.com>
2020-05-01 21:50:31 +0530

    Create 1_subsetSum_recursion.java

---
+++ Dynamic_Prog_(DP)/1_knapSack/2_subset_sum/1_subsetSum_recursion.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    
+    public static boolean ans(int[] arr, int sumLeft, int n){
+        if(sumLeft==0){ return true; }
+        if(n==0){ return false; }
+        if(arr[n-1]>sumLeft){ return ans(arr, sumLeft, n-1); }   // not selected
+        boolean op1 = ans(arr, sumLeft-arr[n-1], n-1);           // selected
+        boolean op2 = ans(arr, sumLeft, n-1);                    // not selected
+        if(op1==true || op2==true)
+            return true;
+        return false;
+    }
+    
+    public static void main(String[] args) throws Exception {
+        int arr[] = {2,8,7,1,5};
+        int sumLeft = 14;
+        System.out.println("Find ans = " + ans(arr, sumLeft, arr.length));
+    }
+}
+
+// ans = true
