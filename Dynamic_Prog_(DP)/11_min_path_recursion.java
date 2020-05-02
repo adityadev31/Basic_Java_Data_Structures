1eed8ba567a224f0cfc5eba269856311f5f505f8
AdiTya-Dev31 <52311859+AdiTya-Dev31@users.noreply.github.com>
2020-05-02 16:18:15 +0530

    Update 11_min_path_recursion.java

--- Dynamic_Prog_(DP)/11_min_path_recursion.java
+++ Dynamic_Prog_(DP)/11_min_path_recursion.java
@@ -4,7 +4,7 @@ public class Main {
     
     public static int findMinPath(int[][] arr, int row, int col){
         if(row<0 || col<0){ return Integer.MAX_VALUE; }            // base case 
-        if(row==0 && col==0){ return arr[row][col]; }              // base case (if array size =1 then return that array)
+        if(row==0 && col==0){ return arr[row][col]; }              // base case (if array size =1 then return that array element)
         int op1 = findMinPath(arr, row-1, col);                    // up
         int op2 = findMinPath(arr, row, col-1);                    // left
         int op3 = findMinPath(arr, row-1, col-1);                  // up-left
