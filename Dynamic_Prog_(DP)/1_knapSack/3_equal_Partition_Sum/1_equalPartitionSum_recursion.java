501677363fbd93b0afa8aa86d0d436523979d6ce
AdiTya-Dev31 <52311859+AdiTya-Dev31@users.noreply.github.com>
2020-05-02 11:57:50 +0530

    Update 1_equalPartitionSum_recursion.java

--- Dynamic_Prog_(DP)/1_knapSack/3_equal_Partition_Sum/1_equalPartitionSum_recursion.java
+++ Dynamic_Prog_(DP)/1_knapSack/3_equal_Partition_Sum/1_equalPartitionSum_recursion.java
@@ -1,3 +1,10 @@
+/**
+problem - given an array, find whether its possible to divide array into two subsets have their sum as equal.
+HINT - for this the sum of all the elements of the original array must be an even number
+HINT - same as previous subsetSum problem just find for sumLeft = sumOfArray/2
+example = arr[] = {1,5,13,7} => sum = 26 i.e sum/2 = 13   so, find for 13 => {13} {1,5,7} 
+**/
+
 import java.util.*;
 
 public class Main {
@@ -9,8 +16,8 @@ public class Main {
     }
     
     public static boolean ans(int[] arr, int sumLeft, int n){
-        if(sumLeft==0){ return true; }
-        if(n==0){ return false; }
+        if(sumLeft==0){ return true; }  // base case
+        if(n==0){ return false; }       // base case
         if(arr[n-1]>sumLeft){ return ans(arr, sumLeft, n-1); } // not selected
         else{
             boolean op1 = ans(arr, sumLeft-arr[n-1], n-1);     // selected
