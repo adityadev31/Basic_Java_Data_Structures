d8dcd018fe4750472271d2ce5b3c9128f8c39906
AdiTya-Dev31 <52311859+AdiTya-Dev31@users.noreply.github.com>
2020-05-09 10:22:32 +0530

    Create 1_countOfPairOfSubsetsOfGivenDiff.java

---
+++ Dynamic_Prog_(DP)/1_knapSack/6_countOfPairOfSubsetsOfGivenDifference/1_countOfPairOfSubsetsOfGivenDiff.java
@@ -0,0 +1,52 @@
+/*
+ question - find the number of pair of subsets that can be formed having difference of their total = given difference.
+ 
+ NOTE - same as count of subsetSum
+ 
+ but instead of argument sum, our sum is ((total arr[])/2)+diff   that it!!
+*/
+
+
+import java.util.*;
+
+public class Main {
+    
+    // find sum of the original array
+    public static int findSum(int[] arr){
+        int sum = 0;
+        for(int i=0; i<arr.length; i++){ sum += arr[i]; }
+        return sum;
+    }
+    
+    public static int ans(int[] arr, int sum, int n, int[][] store){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=sum; j++){
+                if(arr[i-1]>j){ store[i][j]=store[i-1][j]; }
+                else{
+                    int op1 = store[i-1][j];
+                    int op2 = store[i-1][j-arr[i-1]];
+                    store[i][j] = op1+op2;
+                }
+            }
+        }return store[n][sum];
+    }
+    
+    public static int ans(int[] arr, int sum, int n){
+        int[][] store = new int[n+1][sum+1];
+        // base case initialization
+        for(int j=0; j<=sum; j++){ store[0][j] = 0; }
+        for(int i=0; i<=n; i++){ store[i][0] = 1; }
+        return ans(arr, sum, n, store);
+    }
+    
+    public static void main(String[] args) throws Exception {
+        int arr[] = {1,1,2,3};       // given
+        int diff=1;                  // given
+        int sum = findSum(arr);
+        sum = (sum/2)+diff;          // trick
+        System.out.println("Ans = " + ans(arr, sum, arr.length));
+    }
+}
+
+
+// ans = 3  -->   ({1,1,2}{3}), ({1,2}{1`,3}), ({1`,2},{1,3})
