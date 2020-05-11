d5144a7d03ef2f117f6ca00506f42b8efeed08a6
AdiTya-Dev31 <52311859+AdiTya-Dev31@users.noreply.github.com>
2020-05-11 20:55:45 +0530

    Update 2_LCS_array_recursion.java

--- Dynamic_Prog_(DP)/3_LCS_LongestCommonSubsequence/1_LCS/1_LCSubsequence/2_LCS_array_recursion.java
+++ Dynamic_Prog_(DP)/3_LCS_LongestCommonSubsequence/1_LCS/1_LCSubsequence/2_LCS_array_recursion.java
@@ -1,1 +1,31 @@
-xxx
+/**
+Longest Common Subsequence (LCS) - Aditya Hurditya = 5(ditya)
+Using Array and Recursion
+**/
+
+
+import java.util.*;
+
+public class Main {
+
+    public static int LCS(String[] s1, String[] s2, int n, int m){
+        // base case
+        if(n==0 || m==0){ return 0;}
+        // choice case
+        if(s1[n-1]==s2[m-1]){ return (1+LCS(s1, s2, n-1, m-1)); } // char match
+        else{
+            int op1 = LCS(s1, s2, n, m-1);  // select char of s1 leave s2
+            int op2 = LCS(s1, s2, n-1, m);  // select char of s2 leave s1
+            int max = (op1>op2)?op1:op2;
+            return max;
+        }
+    }
+    
+    public static void main(String[] args) throws Exception {
+        String[] s1 = {"A","D","I","T","Y","A"};
+        String[] s2 = {"H","U","R","D","I","T","Y","A"};
+        System.out.println("Substring = " + LCS(s1, s2, s1.length, s2.length));
+    }
+}
+
+// ans = 5
