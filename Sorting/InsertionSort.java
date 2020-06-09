37e1ee98f736d2ed7c99f60463c59188d004e743
AdiTya-Dev31 <52311859+AdiTya-Dev31@users.noreply.github.com>
2020-06-09 21:07:04 +0530

    Create InsertionSort.java

---
+++ Sorting/InsertionSort.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Main {
+    
+    public static void swap(int arr[], int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    
+    public static void main(String[] args) throws Exception {
+        int arr[] = {3,2,5,4,1};
+        
+        // [2,3,5],4,1     we will sort such that left part is sorted 
+        
+        if(arr.length>1){
+            int n = arr.length;
+            if(n>1){
+                for(int i=1; i<n; i++){
+                    int temp = arr[i];
+                    int j=i-1;
+                    while(j>=0 && arr[j]>arr[i]){ j--; }    // finding the first smaller value from right to left
+                    j++;                                    // finally j will become one less hence j++
+                    int mover = i;
+                    while(mover>j){
+                        arr[mover] = arr[mover-1];
+                        mover--;
+                    }
+                    arr[j] = temp;
+                }
+            }
+        }
+        
+        for(int i=0; i<arr.length; i++){ System.out.print(arr[i] + " "); }
+    }
+}
