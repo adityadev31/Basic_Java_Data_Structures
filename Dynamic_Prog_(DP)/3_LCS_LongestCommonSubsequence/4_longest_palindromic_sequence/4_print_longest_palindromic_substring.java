ca1cade47cff9570a52e052609fe8b0d75ce8bdd
AdiTya-Dev31 <52311859+AdiTya-Dev31@users.noreply.github.com>
2020-06-28 18:00:44 +0530

    Update 4_print_longest_palindromic_substring.java

--- Dynamic_Prog_(DP)/3_LCS_LongestCommonSubsequence/4_longest_palindromic_sequence/4_print_longest_palindromic_substring.java
+++ Dynamic_Prog_(DP)/3_LCS_LongestCommonSubsequence/4_longest_palindromic_sequence/4_print_longest_palindromic_substring.java
@@ -18,9 +18,9 @@ public class Main {
         Result result = new Result();
         for(int i=1; i<=n; i++){
             for(int j=1; j<=m; j++){
-                if(s1.charAt(i-1)==s2.charAt(j-1)){
+                if(s1.charAt(i-1)==s2.charAt(j-1)){		// if char matches then store + 1 and compare with result
                     store[i][j] = 1 + store[i-1][j-1];
-                    if(store[i][j] > result.len){
+                    if(store[i][j] > result.len){      // resetting result with our new answers if store > result
                         result.len = store[i][j];
                         result.i = i;
                         result.j = j;
@@ -30,7 +30,7 @@ public class Main {
             }
         }
         
-        if(result.len == 0) return "no match";
+        if(result.len == 0) return "no match";   // just in case
         
         StringBuilder sb = new StringBuilder();
         
@@ -38,22 +38,22 @@ public class Main {
         while(i>=0 && j>=0){
             if(store[i][j]==0) break;
             else{
-                sb.append(s1.charAt(i-1));
+                sb.append(s1.charAt(i-1));  // i-1 coz store[][] was actually i+1 && j+1
                 i--; j--;
             }
         }
+        
         String check1 = sb.toString();
         String check2 = sb.reverse().toString();
         if(check1.equals(check2)) return check1;   // checking if check1 == check2
         else return s1.substring(0,1);             // if no then return Ist char of s1
     }
-    
-    
+     
     public static String LCS(String s1, String s2){
         int n1 = s1.length(), n2 = s2.length();
         int[][] store = new int[n1+1][n2+1];
-        for(int i=0; i<=n1; i++){ store[i][0]=0; }
-        for(int j=0; j<=n2; j++){ store[0][j]=0; }
+        for(int i=0; i<=n1; i++){ store[i][0]=0; }  // initializing base case
+        for(int j=0; j<=n2; j++){ store[0][j]=0; }  // initializing base case
         return LCS(s1, s2, n1, n2, store);
     }
     
