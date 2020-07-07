b8af1ddef5ae916d076ee9b8334cf98a3068cd9b
AdiTya-Dev31 <52311859+AdiTya-Dev31@users.noreply.github.com>
2020-07-07 19:26:27 +0530

    Create 1_egg_drop_recursive.java

---
+++ Dynamic_Prog_(DP)/4_Matrix_chain_multiplication/4_Egg_drop_problem/1_egg_drop_recursive.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.Math;
+
+public class Main {
+    
+    public static int EGG(int egg, int floor){
+        if(floor == 0 || floor == 1) return floor;     // base case
+        if(egg == 1) return floor;                     // base case
+        int min = Integer.MAX_VALUE;
+        for(int k=1; k<=floor; k++){                   // partition
+            int temp = 1 + Math.max(EGG(egg-1, k-1), EGG(egg, floor-k));   // max coz worst case is asked
+            min = Math.min(temp, min);                                     // min coz min no. of attempts is asked
+        }
+        return min;
+    }
+    
+    public static void main(String[] args) throws Exception {
+        int egg = 3, floor = 15;
+        System.out.println(EGG(egg, floor));
+    }
+}
