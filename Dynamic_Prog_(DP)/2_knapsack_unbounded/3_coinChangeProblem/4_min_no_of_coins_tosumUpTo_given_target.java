f9215dcbb8c8ed45dfcd4cf46709fa6e3e8120b9
AdiTya-Dev31 <52311859+AdiTya-Dev31@users.noreply.github.com>
2020-07-13 14:30:07 +0530

    Create 4_min_no_of_coins_tosumUpTo_given_target.java

---
+++ Dynamic_Prog_(DP)/2_knapsack_unbounded/3_coinChangeProblem/4_min_no_of_coins_tosumUpTo_given_target.java
@@ -0,0 +1,50 @@
+/**
+
+    given coins[] = {5,5,2,3}
+    
+    find min coins to full fill sum = 7
+    
+    if not then return -1
+    
+    possibilities:- (5,2) (1,1,1,1,1,1,1) (2,2,2,1) and so on..  so req ans = 2 coz (5,2);
+
+**/
+
+
+
+import java.util.*;
+
+public class Main {
+    
+    public static int minCoins(int[] arr, int n, int sum, int[][] store){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=sum; j++){
+                if(arr[i-1] > j) store[i][j] = store[i-1][j];
+                else store[i][j] = Math.min(store[i-1][j], 1+store[i][j-arr[i-1]]);
+            }
+        }
+        if(store[n][sum] > 100000) return -1;
+        return store[n][sum];
+    }
+    
+    public static int minCoins(int[] arr, int n, int sum){
+        int[][] store = new int[n+1][sum+1];
+        for(int j=0; j<=sum; j++) store[0][j] = Integer.MAX_VALUE-n; // if no items then min req coins = max_int **
+        for(int i=0; i<=n; i++) store[i][0] = 0;                     // sum = 0  then min req coins = 0
+        return minCoins(arr, n, sum, store);
+    }
+    
+    public static void main(String[] args) throws Exception {
+        int coins[] = {5,5,2,3};
+        int sum = 7;
+        System.out.println(minCoins(coins, coins.length, sum));
+    }
+}
+
+// ans = 2
+
+// if array = {5,5,7,15} and sum = 11 --> ans = -1
+
+//    ** Integer.MAX_VALUE + 1 = Integer.MIN_VALUE hence we have to take little less than max   ;)   
+//    we have filled max int i=0 coz we have to find min        see if we had to find max then we would fill it with min
+ 
