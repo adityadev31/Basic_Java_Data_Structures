799875693871e0355499a43f79d2dc24e7a4e9f8
AdiTya Dev <52311859+AdiTya-Dev31@users.noreply.github.com>
2020-10-05 12:57:31 +0530

    Update 3 Insertion Sort.java

--- SORTING_ALGOS/3 Insertion Sort.java
+++ SORTING_ALGOS/3 Insertion Sort.java
@@ -38,4 +38,6 @@ public class Main {
         Time Complexity: O(n*2)
 
         Auxiliary Space: O(1)
+        
+        Boundary Cases: Insertion sort takes maximum time to sort if elements are sorted in reverse order. And it takes minimum time (Order of n) when elements are already sorted.
 */
