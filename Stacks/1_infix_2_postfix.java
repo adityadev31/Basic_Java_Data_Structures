209b00461c0fba20cf8d29f16c1e1970031301be
AdiTya Dev <52311859+AdiTya-Dev31@users.noreply.github.com>
2020-10-10 14:53:57 +0530

    Update 1_infix_2_postfix.java

--- Stacks/1_infix_2_postfix.java
+++ Stacks/1_infix_2_postfix.java
@@ -1,3 +1,74 @@
+import java.util.*;
+
+public class Main{
+    
+    public static int precedence(char c){
+        switch(c){
+            case '+':
+            case '-':
+                return 1;
+                
+            case '*':
+            case '/':
+                return 2;
+                
+            case '^':
+                return 3;
+        }
+        return -1;
+    }
+    
+    public static String postfix(char[] c){
+        Stack<Character> st = new Stack<>();
+        String result = "";
+        
+        for(int i=0; i<c.length; i++){
+            
+            // ====== step1 ========  operator
+            if(precedence(c[i])>0){
+                while(!st.empty() && precedence(st.peek())>=precedence(c[i])) result += st.pop();
+                st.push(c[i]);
+            }
+            
+            // ====== step2 ======== (
+            else if(c[i] == '(') st.push(c[i]);
+            
+            // ====== step3 ======== )
+            else if(c[i] == ')'){
+                while(!st.empty() && st.peek()!='(') result += st.pop();
+                st.pop();
+            }
+            
+            // ====== step4 ======== alphabets
+            else result += c[i];
+            
+        }
+        
+        while(!st.empty()) result += st.pop();
+        return result;
+    }
+    
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        String exp = scan.nextLine();
+        char[] c = exp.toCharArray();
+        System.out.println(postfix(c));
+    }
+    
+}
+
+/*
+    input  - A+B*(C^D-E)
+    output - ABCD^E-*+
+
+*/
+
+
+
+
+/*
+
+
 import java.util.Stack;
 
 public class InfixToPostFix {
@@ -54,3 +125,4 @@ public class InfixToPostFix {
         System.out.println("Postfix Expression: " + infixToPostFix(exp));
     }
 }
+*/
