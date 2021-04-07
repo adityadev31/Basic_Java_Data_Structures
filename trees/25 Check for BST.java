2840d0464e798df6cc9244432fc3c3a65b0715ed
adityadev31 <52311859+adityadev31@users.noreply.github.com>
2021-04-07 11:12:50 +0530

    Update 25 Check for BST.java

--- trees/25 Check for BST.java
+++ trees/25 Check for BST.java
@@ -3,8 +3,9 @@
 
   A P P R O A C H -
   
-  step1 - apply inorder and store root data in an arraylist
-  step2 - check if arraylist is in sorted order or not (i.e, a[i+1] <= a[i]
+  if root == null => return true
+  if (root.data > max) || (root.data < min => false)
+  for left call update max and vice versa
 
 **/
 
@@ -14,20 +15,13 @@ public class Tree
 {
     static List<Integer> list = new ArrayList<>();
     
-    void inorder(Node root){
-        if(root == null) return;
-        inorder(root.left);
-        list.add(root.data);
-        inorder(root.right);
+    boolean isBSTUtil(Node root, int min, int max) {
+        if(root == null) return true;
+        if(root.data < min || root.data > max) return false;
+        return (isBSTUtil(root.left, min, root.data-1) && isBSTUtil(root.right, root.data+1, max));
     }
     
-    boolean isBST(Node root)
-    {
-        list.clear();
-        inorder(root);
-        for(int i=0; i<list.size()-1; i++){
-            if(list.get(i) >= list.get(i+1)) return false;
-        }
-        return true;
+    boolean isBST(Node root) {
+        return isBSTUtil(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
     }
 }
